d7a167644f038378440f60d9430ac9d403d9a821
frnkwang <frnkwang@umich.edu>
2021-10-25 20:22:07 -0400

    Initial commit

---
+++ app/src/main/java/com/example/sortingproject/MainActivity.java
@@ -0,0 +1,50 @@
+package com.example.sortingproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    private int[] myList;
+    private int swap1Index, swap2Index;
+
+    public void increaseIndex1(View view) {
+
+    }
+
+    public void increaseIndex2(View view) {
+
+    }
+
+    public void swapSelected(View view) {
+
+    }
+
+    public void createNewList(View view) {
+
+    }
+
+    public void checkAnswer(View view) {
+
+    }
+
+    private void shuffleList() {
+
+    }
+
+    private boolean isOrdered() {
+        return false;
+    }
+
+    private void updateListDisplay() {
+
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
